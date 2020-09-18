f726fe1297706fc376181eb6b90a642b76925e57
WAQAR AHMAD <waqargpgc@gmail.com>
2020-09-18 17:12:11 +0500

    add loader and tost lab

--- src/main.js
+++ src/main.js
@@ -20,13 +20,35 @@ import App from './App.vue';
 import router from './router';
 import NowUiKit from './plugins/now-ui-kit';
 import VueMoment from 'vue-moment'
-import { BootstrapVue } from 'bootstrap-vue'
+import { BootstrapVue } from 'bootstrap-vue';
+import Toasted from 'vue-toasted';
+import Loading from 'vue-loading-overlay';
+// Import stylesheet
+import 'vue-loading-overlay/dist/vue-loading.css';
+
+let option = {
+    theme: "bubble", 
+    position: "top-center", 
+    duration : 5000
+}
+Vue.use(Toasted,option);
+
+Vue.toasted.register('my_messges',
+    (payload) => {
+        // if there is no message passed show default message
+        if(! payload.message) {
+    	    return "Oops.. Something Went Wrong.."
+        }
+        // if there is a message show it with the message
+        return "Oops.. " + payload.message;
+    },
+);
 
 Vue.config.productionTip = false;
 Vue.use(VueMoment)
 Vue.use(BootstrapVue)
 Vue.use(NowUiKit);
-
+Vue.use(Loading);
 new Vue({
   router,
   render: h => h(App)
--- src/pages/account/Login.vue
+++ src/pages/account/Login.vue
@@ -1,50 +1,52 @@
 <template>
-  <div class="page-header clear-filter" filter-color="orange">
-    <div class="page-header-image" style="background-image: url('img/login.jpg')"></div>
-    <div class="content">
-      <div class="container">
-        <div class="col-md-5 ml-auto mr-auto">
-          <card type="login" plain>
-            <div slot="header" class="logo-container">
-              <img v-lazy="'img/now-logo.png'" alt />
-            </div>
-
-            <fg-input
-              v-model="login.email"
-              class="no-border input-lg"
-              addon-left-icon="now-ui-icons ui-1_email-85"
-              placeholder="Email..."
-            ></fg-input>
-
-            <fg-input
-              v-model="login.password"
-              type="password"
-              class="no-border input-lg"
-              addon-left-icon="now-ui-icons ui-1_lock-circle-open"
-              placeholder="Password..."
-            ></fg-input>
-
-            <template slot="raw-content">
-              <div class="card-footer text-center">
-                <a
-                  @click.prevent="LoginUser"
-                  class="btn btn-primary btn-round btn-md btn-block"
-                >Get Started</a>
+  <div class="vld-parent">
+    <div class="page-header clear-filter" filter-color="orange">
+      <div class="page-header-image" style="background-image: url('img/login.jpg')"></div>
+      <div class="content">
+        <div class="container">
+          <div class="col-md-5 ml-auto mr-auto">
+            <card type="login" plain>
+              <div slot="header" class="logo-container">
+                <img v-lazy="'img/now-logo.png'" alt />
               </div>
-              <div class="pull-left">
-                <router-link to="/signup">
+
+              <fg-input
+                v-model="login.email"
+                class="no-border input-lg"
+                addon-left-icon="now-ui-icons ui-1_email-85"
+                placeholder="Email..."
+              ></fg-input>
+
+              <fg-input
+                v-model="login.password"
+                type="password"
+                class="no-border input-lg"
+                addon-left-icon="now-ui-icons ui-1_lock-circle-open"
+                placeholder="Password..."
+              ></fg-input>
+
+              <template slot="raw-content">
+                <div class="card-footer text-center">
+                  <a
+                    @click.prevent="LoginUser"
+                    class="btn btn-primary btn-round btn-md btn-block"
+                  >Get Started</a>
+                </div>
+                <div class="pull-left">
+                  <router-link to="/signup">
+                    <h6>
+                      <a class="link footer-link">Create Account</a>
+                    </h6>
+                  </router-link>
+                </div>
+                <div class="pull-right">
                   <h6>
-                    <a class="link footer-link">Create Account</a>
+                    <a href="#pablo" class="link footer-link">Need Help?</a>
                   </h6>
-                </router-link>
-              </div>
-              <div class="pull-right">
-                <h6>
-                  <a href="#pablo" class="link footer-link">Need Help?</a>
-                </h6>
-              </div>
-            </template>
-          </card>
+                </div>
+              </template>
+            </card>
+          </div>
         </div>
       </div>
     </div>
@@ -67,24 +69,35 @@ export default {
         email: "",
         password: "",
       },
+      fullPage: false,
       UserInfo: {},
     };
   },
   mounted() {
-   localStorage.clear();
+    localStorage.clear();
   },
   methods: {
     LoginUser() {
+      let loader = this.$loading.show({
+        container: this.fullPage,
+        canCancel: false,
+      });
       AccountService.loginUser(this.login).then((res) => {
         this.UserInfo = res.data;
         if (res.success) {
+          loader.hide();
           localStorage.setItem("token", this.UserInfo.user.token);
           localStorage.setItem("_id", this.UserInfo.user._id);
-          alert(res.message);
-          this.$router.push('/index');
-           window.location.reload();
+          this.$toasted.global.my_messges();
+          this.$router.push("/index");
+          window.location.reload();
         } else {
-          alert(res.message);
+          loader.hide();
+          if (!res.message) {
+            this.$toasted.global.my_messges();
+          } else {
+            this.$toasted.global.my_messges({ message: res.message });
+          }
         }
       });
     },
